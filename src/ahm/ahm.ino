07b7ac227742ab16e40d389a7d685f7110179393
vdemay <vincent@demay-fr.net>
2012-11-23 14:55:26 +0100

    new code

--- src/ahm/ahm.ino
+++ src/ahm/ahm.ino
@@ -1,66 +1,64 @@
-// http://www.arduino.cc/en/Tutorial/LiquidCrystal
-#include <LiquidCrystal.h>
+#include <DS1307RTC.h>
+#include <Time.h>  
 #include <Wire.h>
-#include "RTClib.h"
+#include <LiquidCrystal.h>
 
 // initialize the library with the numbers of the interface pins
-// TODO LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-String currentDate;   
-int zone1_Output = 9;
-int zone2_Output = 10;
-int zone3_Output = 11;
-int zone4_Output = 12;
+LiquidCrystal lcd(9, 8, 5, 4, 3, 2);
 
 void setup() {
-  // -- LDCD PART
+  Serial.begin(9600);
+  Serial.println("[Booting...]"); 
+  
+  //time
+  Serial.print("RTC Init");
+  setSyncProvider(RTC.get);   // the function to get the time from the RTC
+  if(timeStatus()!= timeSet) {
+    Serial.println("\t\t\t[NOK]");
+    Serial.println("Unable to sync with the RTC");
+  } else {
+    Serial.println("\t\t\t[OK]");   
+  }
+  //RTC.set(1353506130);   // set the RTC and the system time to the received value
+  //setTime(1353506130);
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("Starting AHM...);
+  lcd.print("hello, world!");
   
-  // -- RTC PART
-  Serial.begin(57600);
-  Wire.begin();
-  RTC.begin();
-  if (! RTC.isrunning()) {
-    lcd.print("RTC is NOT running!");
-    // following line sets the RTC to the date & time this sketch was compiled
-    //RTC.adjust(DateTime(__DATE__, __TIME__));
-    return;
-  }
-  // reserve 200 bytes for the currentDate:
-  currentDate.reserve(200);
-  
-  // -- OUTPUT PART
-  pinMode(zone1_Output, OUTPUT);
-  pinMode(zone2_Output, OUTPUT);
-  pinMode(zone3_Output, OUTPUT);
-  pinMode(zone4_Output, OUTPUT);
 }
 
 void loop() {
-  // Get current date from the RTC
-  DateTime now = RTC.now();
-  // Print the date on the LCD
-  lcd.print(getCurrentDateAsString(now));
-  
-
-
-  // Wait 3s & restart the loop...
-  delay(3000);
+   // digitalClockDisplay();
+  delay(1000);
+  lcd.setCursor(0, 1);
+  lcd.print(hour());
+  lcd.setCursor(2, 1);
+  lcd.print(":");
+  lcd.setCursor(3, 1);
+  lcd.print(minute());
+  lcd.print(":");
+  lcd.setCursor(6, 1);
+  lcd.print(second());
 }
 
-String getCurrentDateAsString(DateTime now) {
-  // Re-init...
-  currentDate = "";
-  currentDate += now.weekday();
-  currentDate += " ";
-  currentDate += now.day();
-  currentDate += " ";
-  currentDate += now.month();
-  currentDate += " ";
-  currentDate += now.hour();
-  currentDate += ":";
-  currentDate += now.minute();
-  
-} 
+void digitalClockDisplay(){
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year()); 
+  Serial.println(); 
+}
+
+void printDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
