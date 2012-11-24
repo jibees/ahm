2a209cee6b323e8a401903fbaf6a7f3950e4ccde
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2012-11-24 19:57:55 +0100

    New version

--- src/ahm/ahm.ino
+++ src/ahm/ahm.ino
@@ -6,59 +6,200 @@
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(9, 8, 5, 4, 3, 2);
 
+// Timezone shifting : we are in Europe/Paris ie. +1 
+const int timezoneShiftInHour = 1;
+
+const int zone1Output = 10;
+const int zone2Output = 11;
+const int zone3Output = 12;
+
+const int modeEco = HIGH;
+const int modeConfort = LOW;
+
+int zone1ModeValue = modeConfort;
+int zone2ModeValue = modeConfort;
+int zone3ModeValue = modeConfort;
+
 void setup() {
   Serial.begin(9600);
-  Serial.println("[Booting...]"); 
-  
-  //time
-  Serial.print("RTC Init");
-  setSyncProvider(RTC.get);   // the function to get the time from the RTC
-  if(timeStatus()!= timeSet) {
-    Serial.println("\t\t\t[NOK]");
-    Serial.println("Unable to sync with the RTC");
-  } else {
-    Serial.println("\t\t\t[OK]");   
-  }
-  //RTC.set(1353506130);   // set the RTC and the system time to the received value
-  //setTime(1353506130);
-  // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
-  lcd.print("hello, world!");
+  lcd.setCursor(0, 0);
+  lcd.print("Booting..."); 
   
+  lcd.setCursor(0, 0);
+  lcd.print("RTC Init... ");
+  setSyncProvider(RTC.get);
+  if(timeStatus()!= timeSet) {
+    lcd.setCursor(0, 0);
+    lcd.print("Unable to sync with the RTC");
+  } else {
+    lcd.setCursor(0, 0);
+    lcd.print("RTC Init... OK");   
+  }
+  // set the RTC and the system time to the received value
+  //RTC.set(1353775757);  
+  delay(1000);
 }
 
 void loop() {
-   // digitalClockDisplay();
-  delay(1000);
+  manageZone1();
+  manageZone2();
+ 
+  // Update the relais
+  updateTheRelays();
+  // Print the Date on row 1
+  lcd.setCursor(0, 0);
+  lcd.print(getPrintableDate());
+  
+  // Print the zones on row 2
   lcd.setCursor(0, 1);
-  lcd.print(hour());
-  lcd.setCursor(2, 1);
-  lcd.print(":");
-  lcd.setCursor(3, 1);
-  lcd.print(minute());
-  lcd.print(":");
-  lcd.setCursor(6, 1);
-  lcd.print(second());
+  lcd.print(getPrintableZones());
+  
+  
+  
+  delay(10000);
 }
 
-void digitalClockDisplay(){
-  // digital clock display of the time
-  Serial.print(hour());
-  printDigits(minute());
-  printDigits(second());
-  Serial.print(" ");
-  Serial.print(day());
-  Serial.print(" ");
-  Serial.print(month());
-  Serial.print(" ");
-  Serial.print(year()); 
-  Serial.println(); 
+void updateTheRelays() {
+  digitalWrite(zone1Output, zone1ModeValue);
+  digitalWrite(zone2Output, zone2ModeValue);
+  digitalWrite(zone3Output, zone3ModeValue);
 }
 
-void printDigits(int digits){
-  // utility function for digital clock display: prints preceding colon and leading 0
-  Serial.print(":");
-  if(digits < 10)
-    Serial.print('0');
-  Serial.print(digits);
+// CHAMBRE DE MATILDA
+void manageZone1() {
+  switch (weekday()) {
+    case 1:
+    case 7:
+      // Weekend, Saturday, Sunday
+      // Always on
+      zone1ModeValue = modeConfort;
+      break;
+    default:
+      // On week
+      // modeConfort : 17h-8h
+      int realHour = hour() + timezoneShiftInHour;
+      if (realHour < 8 || realHour > 17) {
+        zone1ModeValue = modeConfort;
+      } else {
+        zone1ModeValue = modeEco;
+      }
+      break;
+  } 
+}
+
+
+// SALON
+void manageZone3() {
+  switch (weekday()) {
+    case 1:
+    case 7:
+      // Weekend, Saturday, Sunday
+      // Always on
+      zone1ModeValue = modeConfort;
+      break;
+    default:
+      // On week
+      // modeConfort : 17h-8h
+      int realHour = hour() + timezoneShiftInHour;
+      if (17 < realHour < 23 || 7 < realHour < 9 ) {
+        zone1ModeValue = modeConfort;
+      } else {
+        zone1ModeValue = modeEco;
+      }
+      break;
+  } 
+}
+
+// CHAMBRE DES PARENTS
+void manageZone2() {
+  // modeConfort 22h-23h
+  int realHour = hour() + timezoneShiftInHour;
+  if (22 < realHour  && realHour <= 23) {
+    zone2ModeValue = modeConfort;
+  } else {
+    zone2ModeValue = modeEco;
+    Serial.print(zone2ModeValue);
+  }
+}
+
+
+String getPrintableZones() {
+  String printableZones = String();
+  
+  // ZONE 1
+  printableZones = "1:";
+  printableZones += getModeStringValue(zone1ModeValue);
+  // ZONE 2
+  printableZones += " ";
+  printableZones += "2:";
+  printableZones += getModeStringValue(zone2ModeValue);
+  // ZONE 3
+  printableZones += " ";
+  printableZones += "3:";
+  printableZones += getModeStringValue(zone3ModeValue);
+  return printableZones;
+}
+ 
+String getModeStringValue(int zoneModeValue) {
+  switch(zoneModeValue) {
+   case modeEco:
+     return "Ec";
+   case modeConfort:
+     return "Co";
+  }   
+}
+
+String getPrintableDate() {
+   // Lu 23 11 16:03:09
+  String printableDate = String();
+  // Day Of Week
+  printableDate = String(getDayOfWeek(weekday())); 
+  // Day in Month
+  printableDate += " "; 
+  printableDate += String(day());
+  // Month
+  printableDate += "/"; 
+  printableDate += String(month());
+  
+  // Hour
+  printableDate += " ";
+  printableDate += printDigitForDate(hour()+timezoneShiftInHour);
+  // Minute
+  printableDate += ":";
+  printableDate += printDigitForDate(minute());
+  
+  //Serial.println(printableDate);
+  return printableDate;
+}
+
+String getDayOfWeek(int weekday){
+  switch(weekday) {
+    case 1:
+      return "Sun";
+    case 2:
+      return "Mon";
+    case 3:
+      return "Tue";
+    case 4:
+      return "Wed";
+    case 5:
+      return "Thu";
+    case 6:
+      return "Fri";
+    case 7:
+      return "Sat";
+  }
+}
+
+String printDigitForDate(int digits) {
+  String digitsString = String();
+  if (digits < 10) {
+    digitsString = "0";
+    digitsString += String(digits);
+  } else {
+    digitsString = String(digits);
+  }
+  //Serial.println(digitsString);
+  return digitsString;
 }
